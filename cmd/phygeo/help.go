474ebd559363b9b9e1730ce30b65427e91317714
J. Salvador Arias <jsalarias@gmail.com>
2023-11-24 14:24:59 -0300

    cmd/phygeo: add a help guide abut tree files

--- cmd/phygeo/help.go
+++ cmd/phygeo/help.go
@@ -9,6 +9,7 @@ import "github.com/js-arias/command"
 func init() {
 	app.Add(pixelPriorGuide)
 	app.Add(projectsGuide)
+	app.Add(treeFilesGuide)
 }
 
 var projectsGuide = &command.Command{
@@ -97,4 +98,40 @@ Here is an example file:
 
 In this case, the comment column will be ignored.
 `,
+}
+
+var treeFilesGuide = &command.Command{
+	Usage: "tree-files",
+	Short: "about tree files",
+	Long: `
+In PhyGeo, phylogenetic trees must be time-calibrated and stored in a
+tab-delimited file. The advantage of using a tab-delimited file is that it
+would be easier to manipulate trees than in traditional newick files; for
+example, it would be easier for commands in PhyGeo, as well as for third-party
+applications, to understand the node IDs.
+
+The recommended way to interact with time-calibrated trees in a PhyGeo project
+is by using the commands in "phygeo tree".
+	
+A PhyGeo tree file is a tab-delimited file with the following columns:
+	
+	-tree    for the name of the tree.
+	-node    for the ID of the node.
+	-parent  for of ID of the parent node (-1 is used for the root).
+	-age     the age of the node (in years).
+	-taxon   the taxonomic name of the node.
+	
+Here is an example file:
+
+	# time calibrated phylogenetic tree
+	tree	node	parent	age	taxon
+	dinosaurs	0	-1	235000000
+	dinosaurs	1	0	230000000	Eoraptor lunensis
+	dinosaurs	2	0	170000000
+	dinosaurs	3	2	145000000	Ceratosaurus nasicornis
+	dinosaurs	4	2	71000000	Carnotaurus sastrei
+	
+In a PhyGeo project, the file that contains the trees is indicated with the
+"trees" keyword.
+	`,
 }
