6e3a9f11dd10667bece0df9655531a0d72a5eed6
J. Salvador Arias <jsalarias@gmail.com>
2023-12-12 11:46:51 -0300

    cmd/phygeo: refactor help guides
    Now help guides are moved to the respective command collection
    (for example,
    the guide for the tree file format is now
    in the trees command collection).

--- cmd/phygeo/diff/diff.go
+++ cmd/phygeo/diff/diff.go
@@ -31,4 +31,59 @@ func init() {
 	Command.Add(ml.Command)
 	Command.Add(particles.Command)
 	Command.Add(speed.Command)
+
+	// help topics
+	Command.Add(pixProbGuide)
+}
+
+var pixProbGuide = &command.Command{
+	Usage: "pix-prob-files",
+	Short: "pixel probability files",
+	Long: `
+Pixel probability files are used in PhyGeo to store the particular probability
+of a pixel in a node at a given time stage. In PhyGeo, the main usage of this
+kind of file is as an input to build the reconstruction maps.
+
+A pixel probability file is a tab-delimited file with the following columns:
+
+	-tree     the name of the tree	
+	-node     the ID of the node
+	-age      the age of the time stage, in years
+	-type     the type of the stored probability. It can be "log-like" for
+	          log-likelihood values (for example, the output of the
+	          "diff like" command), "freq" for the raw frequency of a
+	          pixel, or "kde" for the smoothed frequency of a pixel (both
+	          can be produced by the output of the "diff freq" command).
+	-equator  the number of pixels in the equator of the pixelation
+	-pixel    the ID of the pixel (from the pixelation)
+	-value    the probability value of the pixel.
+
+The file can also include the following columns:
+
+	-lambda  in the log-like files, this column indicates the lambda value
+	         used for the likelihood calculations.
+
+Here are some example files:
+
+	# logLike file
+	tree	node	age	type	lambda	equator	pixel	value
+	vireya	0	18249000	log-like	100.0	120	0	-426.6
+	vireya	0	18249000	log-like	100.0	120	1	-426.9
+	vireya	0	18249000	log-like	100.0	120	2	-427.8
+	vireya	0	18249000	log-like	100.0	120	3	-427.3
+
+	# freq file
+	tree	node	age	type	equator	pixel	value
+	vireya	1	15000000	freq	120	873	0.004000
+	vireya	1	15000000	freq	120	874	0.004000
+	vireya	1	15000000	freq	120	875	0.002000
+	vireya	1	15000000	freq	120	876	0.003000
+
+	# kde file
+	vireya	2	15000000	kde	120	1609	0.035273
+	vireya	2	15000000	kde	120	1610	0.075713
+	vireya	2	15000000	kde	120	1611	0.162439
+	vireya	2	15000000	kde	120	1612	0.337214
+	vireya	2	15000000	kde	120	1613	0.255504
+	`,
 }
--- cmd/phygeo/geo/geo.go
+++ cmd/phygeo/geo/geo.go
@@ -20,4 +20,134 @@ var Command = &command.Command{
 func init() {
 	Command.Add(add.Command)
 	Command.Add(prior.Command)
+
+	// help guides
+	Command.Add(landscapeModelGuide)
+	Command.Add(motionModelGuide)
+	Command.Add(pixelPriorGuide)
+}
+
+var landscapeModelGuide = &command.Command{
+	Usage: "landscape",
+	Short: "about landscape models",
+	Long: `
+A landscape model (or paleolandscape model) stores pixel values at different
+times. These values are usually associated with a particular landscape feature
+(for example, 1 is associated with shallow seas, while 5 is associated with
+high-altitude mountains). In PhyGeo, the landscape models are stored as
+tab-delimited files.
+
+There are two uses for the landscape models in PhyGeo. The first, and most
+important, is for the analysis. The landscape features are associated with
+pixel priors. Therefore, the landscape feature modifies the probability of
+assigning a pixel as part of the ancestral range. In the second usage, mapping
+commands use feature values to assign background colors.
+
+The landscape models are closely associated with plate motion models. In
+PhyGeo, both models should have the same spatial and temporal resolution. They
+are kept separated for flexibility; for example, you can have two different
+landscape models using the same plate motion model.
+
+Landscape models are taken as given in PhyGeo. If you want to work with
+landscape models, the recommended way is to use the tools 'plates' or
+'platesgui' available at: <https://github.com/js-arias/earth>. Here is a
+repository with a collection of landscape models:
+<https://github.com/js-arias/geomodels>.
+
+A landscape model is a tab-delimited file with the following columns:
+
+	-equator      for the size of the pixelation (the number of pixels in
+	              the equatorial ring).
+	-age          the time stage, in years.	
+	-stage-pixel  the ID of a pixel at the indicated time stage.
+	-value         an integer value associated with a landscape feature.
+
+Here is an example file:
+
+	equator	age	stage-pixel	value
+	360	100000000	19051	1
+	360	100000000	19055	2
+	360	100000000	19409	1
+	360	140000000	20051	1
+	360	140000000	20055	2
+	360	140000000	20056	3
+
+In a PhyGeo project, the file that contains the landscape model is indicated
+with the "landscape" keyword.
+	`,
+}
+
+var motionModelGuide = &command.Command{
+	Usage: "motion-model",
+	Short: "about plate motion models",
+	Long: `
+A plate motion model stores the locations of tectonic features at different
+times. While geologists use a vectorial and time-continuous rotation model, in
+PhyGeo, a rasterized and time-discrete version of such models is stored as a
+tab-delimited file.
+
+Plate motion models are taken as given in PhyGeo. If you want to work with
+plate motion models, the recommended way is to use the tool 'plates',
+available at: <https://github.com/js-arias/earth>. Here is a repository with a
+collection of plate motion models: <https://github.com/js-arias/geomodels>.
+
+A plate motion model file is a tab-delimited file with the following columns:
+
+	-equator      for the size of the pixelation (the number of pixels in
+	              the equatorial ring).
+	-plate        the ID of the tectonic plate.
+	-pixel        the ID of the pixel at the present location (from the
+	              pixelation).
+	-age          the time stage, in years.
+	-stage-pixel  the ID of the pixel at the indicated time stage.
+	
+Here is an example file:
+
+	equator	plate	pixel	age	stage-pixel
+	360	59999	17051	100000000	19051
+	360	59999	17051	140000000	20051
+	360	59999	17055	100000000	19055
+	360	59999	17055	140000000	20055
+	360	59999	17055	140000000	20056
+
+In a PhyGeo project, the file that contains the plate motion model is
+indicated with the "geomotion" keyword.
+	`,
+}
+
+var pixelPriorGuide = &command.Command{
+	Usage: "pixel-priors",
+	Short: "about the pixel prior files",
+	Long: `
+To take into account the landscape, each pixel must have a different prior, so
+some pixels will be more likely to be sampled than others based on the
+landscape features.
+
+In PhyGeo, such priors can be defined in a file for pixel prior values. The
+recommended way to interact with the priors is by using the command
+"phygeo geo prior", which can be used to add a pixel prior file, view the
+current priors, or set or edit values. See "phygeo help geo add" to learn
+more.
+  
+A pixel prior file is a tab-delimited file with the following fields:
+  
+	- key    the value used as an identifier in the landscape model must 
+                 be an integer.
+	- prior  the prior of the pixel, it should be a value between 0 and 1.
+  
+Here is an example file:
+
+	key	prior	comment
+	0	0.000000	deep ocean
+	1	0.010000	oceanic plateaus
+	2	0.050000	continental shelf
+	3	0.950000	lowlands
+	4	1.000000	highlands
+	5	0.001000	ice sheets
+
+In this case, the comment column will be ignored.
+
+In a PhyGeo project, the file that contains the pixel priors is indicated with
+the "pixprior" keyword.
+`,
 }
