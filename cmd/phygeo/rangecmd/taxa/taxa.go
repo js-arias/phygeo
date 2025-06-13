72e8cfee1c584fde71ce046ffee85ed20efd475f
J. Salvador Arias <jsalarias@gmail.com>
2025-06-13 15:52:23 +0300

    cmd/phygeo: check for valid ranges when using a random walk model

--- cmd/phygeo/rangecmd/taxa/taxa.go
+++ cmd/phygeo/rangecmd/taxa/taxa.go
@@ -10,19 +10,22 @@ package taxa
 import (
 	"fmt"
 	"io"
-	"os"
 	"slices"
+	"strings"
 
 	"github.com/js-arias/command"
 	"github.com/js-arias/earth/model"
+	"github.com/js-arias/earth/pixkey"
 	"github.com/js-arias/earth/stat/pixweight"
 	"github.com/js-arias/phygeo/project"
+	"github.com/js-arias/phygeo/trait"
 	"github.com/js-arias/ranges"
-	"github.com/js-arias/timetree"
 )
 
 var Command = &command.Command{
-	Usage: "taxa [--count] [--val] <project-file>",
+	Usage: `taxa [-m|--model <model-type>]
+	[--count] [--val]
+	<project-file>`,
 	Short: "print a list of taxa with distribution ranges",
 	Long: `
 Command taxa reads the geographic ranges from a PhyGeo project and print the
@@ -30,15 +33,24 @@ name of the taxa in the standard output.
 
 The argument of the command is the name of the project file.
 
-If the flag --count is defined, the number of valid, the total amount of
-pixels, and the type of the range will be printed in front of each taxon name
-that is defined in at least one tree. To be valid a pixel must be defined over
-a landscape value with a prior probability greater than zero.
+If the flag --count is defined, the number of valid (when a walk model is used
+the number of pixels with low settlement weights will also be in brackets),
+the total amount of pixels, and the type of the range will be printed in front
+of each taxon name that is defined in at least one tree. To be valid a pixel
+must be defined over a landscape value with a weight greater than zero. A low
+weight settlement is a settlement weight below 0.5.
 
 If the flag --val is defined, and all the taxa has valid records, the command
 will finish silently. Otherwise, any invalid taxon (a taxon without valid
 records) will be reported. To be valid, a taxon must have, at least, one
 valid pixel (i.e. a pixel with a weight greater than zero).
+
+By default, it will assume that the project is for a random walk. The model
+only makes difference if --count or -val flags are used. Use the flag --model,
+or -m, to define a different model. Valid values are:
+
+	walk	a random walk (the default)
+	diff	a diffusion model
 	`,
 	SetFlags: setFlags,
 	Run:      run,
@@ -47,11 +59,14 @@ valid pixel (i.e. a pixel with a weight greater than zero).
 var countFlag bool
 var rangeFlag bool
 var valFlag bool
+var modelFlag string
 
 func setFlags(c *command.Command) {
 	c.Flags().BoolVar(&countFlag, "count", false, "")
 	c.Flags().BoolVar(&rangeFlag, "ranges", false, "")
 	c.Flags().BoolVar(&valFlag, "val", false, "")
+	c.Flags().StringVar(&modelFlag, "model", "walk", "")
+	c.Flags().StringVar(&modelFlag, "m", "walk", "")
 }
 
 func run(c *command.Command, args []string) error {
@@ -77,7 +92,7 @@ func run(c *command.Command, args []string) error {
 
 		// all terminal taxa are "invalid":
 		// no distribution data defined.
-		ls, err := makeTermList(tf)
+		ls, err := makeTermList(p)
 		if err != nil {
 			return nil
 		}
@@ -87,7 +102,12 @@ func run(c *command.Command, args []string) error {
 		return nil
 	}
 
-	coll, err := readRanges(rf)
+	landscape, err := p.Landscape(nil)
+	if err != nil {
+		return err
+	}
+
+	coll, err := p.Ranges(landscape.Pixelation())
 	if err != nil {
 		return err
 	}
@@ -96,38 +116,40 @@ func run(c *command.Command, args []string) error {
 	}
 
 	if valFlag || countFlag {
-		lsf := p.Path(project.Landscape)
-		if lsf == "" {
-			msg := fmt.Sprintf("paleolandscape not defined in project %q", args[0])
-			return c.UsageError(msg)
-		}
-		landscape, err := readLandscape(lsf)
-		if err != nil {
-			return err
-		}
-
-		pwF := p.Path(project.PixWeight)
-		if pwF == "" {
-			msg := fmt.Sprintf("pixel weights not defined in project %q", args[0])
-			return c.UsageError(msg)
-		}
-		pw, err := readPixWeights(pwF)
-		if err != nil {
-			return err
-		}
-
-		tf := p.Path(project.Trees)
-		if tf == "" {
-			msg := fmt.Sprintf("tree file not defined in project %q", args[0])
-			return c.UsageError(msg)
-		}
-
-		ls, err := makeTermList(tf)
+		ls, err := makeTermList(p)
 		if err != nil {
 			return nil
 		}
 
-		valCount(c.Stdout(), ls, coll, landscape, pw)
+		modelFlag = strings.ToLower(modelFlag)
+		switch modelFlag {
+		case "diff":
+			pw, err := p.PixWeight()
+			if err != nil {
+				return err
+			}
+
+			valCount(c.Stdout(), ls, coll, landscape, pw)
+		case "walk":
+			keys, err := p.Keys()
+			if err != nil {
+				return err
+			}
+
+			tr, err := p.Traits()
+			if err != nil {
+				return err
+			}
+
+			st, err := p.Settlement(tr, keys)
+			if err != nil {
+				return err
+			}
+
+			valCountWalk(c.Stdout(), ls, coll, landscape, tr, keys, st)
+		default:
+			return fmt.Errorf("unknown model %q", modelFlag)
+		}
 		return nil
 	}
 
@@ -139,61 +161,10 @@ func run(c *command.Command, args []string) error {
 	return nil
 }
 
-func readRanges(name string) (*ranges.Collection, error) {
-	f, err := os.Open(name)
+func makeTermList(p *project.Project) ([]string, error) {
+	c, err := p.Trees()
 	if err != nil {
 		return nil, err
-	}
-	defer f.Close()
-
-	coll, err := ranges.ReadTSV(f, nil)
-	if err != nil {
-		return nil, fmt.Errorf("when reading %q: %v", name, err)
-	}
-
-	return coll, nil
-}
-
-func readLandscape(name string) (*model.TimePix, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	tp, err := model.ReadTimePix(f, nil)
-	if err != nil {
-		return nil, fmt.Errorf("on file %q: %v", name, err)
-	}
-
-	return tp, nil
-}
-
-func readPixWeights(name string) (pixweight.Pixel, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	pw, err := pixweight.ReadTSV(f)
-	if err != nil {
-		return nil, fmt.Errorf("when reading %q: %v", name, err)
-	}
-
-	return pw, nil
-}
-
-func makeTermList(name string) ([]string, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	c, err := timetree.ReadTSV(f)
-	if err != nil {
-		return nil, fmt.Errorf("while reading file %q: %v", name, err)
 	}
 
 	ls := c.Names()
@@ -249,5 +220,57 @@ func valCount(w io.Writer, ls []string, coll *ranges.Collection, tp *model.TimeP
 		}
 
 		fmt.Fprintf(w, "%s\t%d\t%d\t%s\n", tax, val, len(rng), coll.Type(tax))
+	}
+}
+
+func valCountWalk(w io.Writer, ls []string, coll *ranges.Collection, tp *model.TimePix, tr *trait.Data, key *pixkey.PixKey, sett *trait.Matrix) {
+	for _, tax := range ls {
+		if !coll.HasTaxon(tax) {
+			if valFlag {
+				fmt.Fprintf(w, "INVALID TAXON: no records: %s\n", tax)
+				continue
+			}
+			fmt.Fprintf(w, "%s\t%d [%d]\t%d\tNA\n", tax, 0, 0, 0)
+			continue
+		}
+
+		rng := coll.Range(tax)
+		age := tp.ClosestStageAge(coll.Age(tax))
+		lsc := tp.Stage(age)
+		obs := tr.Obs(tax)
+		val := 0
+		high := 0
+		for px := range rng {
+			v := lsc[px]
+			var isVal, isHigh bool
+			for _, t := range obs {
+				w := sett.Weight(t, key.Label(v))
+				if w > 0 {
+					isVal = true
+				}
+				if w > 0.5 {
+					isHigh = true
+				}
+			}
+			if isVal {
+				val++
+			}
+			if isHigh {
+				high++
+			}
+		}
+
+		if valFlag {
+			if val == 0 {
+				fmt.Fprintf(w, "INVALID TAXON: no valid pixels: %s: %d pixels\n", tax, len(rng))
+				continue
+			}
+			if high == 0 {
+				fmt.Fprintf(w, "WARNING: low probability pixels: %s: %d of %d pixels\n", tax, val, len(rng))
+			}
+			continue
+		}
+
+		fmt.Fprintf(w, "%s\t%d [%d]\t%d\t%s\n", tax, val, high, len(rng), coll.Type(tax))
 	}
 }
